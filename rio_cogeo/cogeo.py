fcbc33af871b07f8f3dcb8ab5055752ec94584fa
vincentsarago <vincent.sarago@mapbox.com>
2018-04-03 15:55:24 -0400

    create overview and copy

--- rio_cogeo/cogeo.py
+++ rio_cogeo/cogeo.py
@@ -7,7 +7,9 @@ import click
 import numpy
 
 import rasterio
+from rasterio.io import MemoryFile
 from rasterio.enums import Resampling
+from rasterio.shutil import copy
 
 
 def cog_translate(src, dst, dst_opts,
@@ -39,35 +41,36 @@ def cog_translate(src, dst, dst_opts,
         with rasterio.open(src) as src:
 
             indexes = indexes if indexes else src.indexes
-
             meta = src.meta
-            meta['count'] = len(indexes)
             meta.pop('nodata', None)
             meta.pop('alpha', None)
+            meta['count'] = len(indexes)
             meta.pop('compress', None)
             meta.update(**dst_opts)
 
-            with rasterio.open(dst, 'w', **meta) as dst:
+            with MemoryFile() as memfile:
+                with memfile.open(**meta) as mem:
+                    mask = numpy.zeros((mem.height, mem.width), dtype=numpy.uint8)
+                    wind = list(mem.block_windows(1))
 
-                mask = numpy.zeros((dst.height, dst.width), dtype=numpy.uint8)
-                wind = list(dst.block_windows(1))
+                    with click.progressbar(wind, length=len(wind), file=sys.stderr, show_percent=True) as windows:
+                        for ij, w in windows:
+                            matrix = src.read(window=w, indexes=indexes, boundless=True)
+                            mem.write(matrix, window=w)
 
-                with click.progressbar(wind, length=len(wind), file=sys.stderr, show_percent=True) as windows:
-                    for ij, w in windows:
-                        matrix = src.read(window=w, indexes=indexes, boundless=True)
-                        dst.write(matrix, window=w)
+                            if nodata is not None:
+                                mask_value = numpy.all(matrix != nodata, axis=0).astype(numpy.uint8) * 255
+                            elif alpha is not None:
+                                mask_value = src.read(alpha, window=w, boundless=True)
+                            else:
+                                mask_value = src.dataset_mask(window=w, boundless=True)
 
-                        if nodata is not None:
-                            mask_value = numpy.all(matrix != nodata, axis=0).astype(numpy.uint8) * 255
-                        elif alpha is not None:
-                            mask_value = src.read(alpha, window=w, boundless=True)
-                        else:
-                            mask_value = src.dataset_mask(window=w, boundless=True)
+                            mask[w.row_off:w.row_off + w.height, w.col_off:w.col_off + w.width] = mask_value
 
-                        mask[w.row_off:w.row_off + w.height, w.col_off:w.col_off + w.width] = mask_value
+                    mem.write_mask(mask)
 
-                dst.write_mask(mask)
+                    overviews = [2**j for j in range(1, overview_level + 1)]
+                    mem.build_overviews(overviews, Resampling.nearest)
+                    mem.update_tags(ns='rio_overview', resampling=Resampling.nearest.value)
 
-                overviews = [2**j for j in range(1, overview_level + 1)]
-                dst.build_overviews(overviews, Resampling.nearest)
-                dst.update_tags(ns='rio_overview', resampling=Resampling.nearest.value)
+                    copy(mem, dst, copy_src_overviews=True, **dst_opts)
