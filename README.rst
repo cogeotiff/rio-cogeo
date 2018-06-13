712c083b61918ba2603ec9572fb74347a0dea63c
vincentsarago <vincent.sarago@mapbox.com>
2018-06-13 09:33:27 -0400

    blackify + pre-commit + documentation

--- README.rst
+++ README.rst
@@ -60,6 +60,8 @@ Examples
 Defaults COGEO profiles
 =======================
 
+Profiles can be extended by providing `--co` option in command line (e.g: rio cogeo mydataset.tif mydataset_zstd.tif -b 1,2,3 --profile deflate --co "COMPRESS=ZSTD" )
+
 **YCbCr**
 
 - JPEG compression
@@ -91,3 +93,17 @@ Defaults COGEO profiles
 Defaults profiles are tiled with 512x512 blocksizes
 
 You can update default profile by providing `--co` options in the cli.
+
+Contribution & Devellopement
+============================
+
+Issues and pull requests are more than welcome.
+
+**dev install**
+
+.. code-block:: console
+
+  $ git clone https://github.com/mapbox/rio-cogeo.git
+  $ cd rio-cogeo
+  $ pip install -e .[dev]
+  $ pre-commit install
--- rio_cogeo/cogeo.py
+++ rio_cogeo/cogeo.py
@@ -12,9 +12,16 @@ from rasterio.enums import Resampling
 from rasterio.shutil import copy
 
 
-def cog_translate(src_path, dst_path, dst_kwargs,
-                  indexes=None, nodata=None, alpha=None, overview_level=6,
-                  config=None):
+def cog_translate(
+    src_path,
+    dst_path,
+    dst_kwargs,
+    indexes=None,
+    nodata=None,
+    alpha=None,
+    overview_level=6,
+    config=None,
+):
     """
     Create Cloud Optimized Geotiff.
 
@@ -46,11 +53,11 @@ def cog_translate(src_path, dst_path, dst_kwargs,
 
             indexes = indexes if indexes else src.indexes
             meta = src.meta
-            meta['count'] = len(indexes)
-            meta.pop('nodata', None)
-            meta.pop('alpha', None)
-            meta.pop('compress', None)
-            meta.pop('photometric', None)
+            meta["count"] = len(indexes)
+            meta.pop("nodata", None)
+            meta.pop("alpha", None)
+            meta.pop("compress", None)
+            meta.pop("photometric", None)
             meta.update(**dst_kwargs)
 
             with MemoryFile() as memfile:
@@ -58,24 +65,36 @@ def cog_translate(src_path, dst_path, dst_kwargs,
                     mask = numpy.zeros((mem.height, mem.width), dtype=numpy.uint8)
                     wind = list(mem.block_windows(1))
 
-                    with click.progressbar(wind, length=len(wind), file=sys.stderr, show_percent=True) as windows:
+                    with click.progressbar(
+                        wind, length=len(wind), file=sys.stderr, show_percent=True
+                    ) as windows:
                         for ij, w in windows:
                             matrix = src.read(window=w, indexes=indexes, boundless=True)
                             mem.write(matrix, window=w)
 
                             if nodata is not None:
-                                mask_value = numpy.all(matrix != nodata, axis=0).astype(numpy.uint8) * 255
+                                mask_value = (
+                                    numpy.all(matrix != nodata, axis=0).astype(
+                                        numpy.uint8
+                                    )
+                                    * 255
+                                )
                             elif alpha is not None:
                                 mask_value = src.read(alpha, window=w, boundless=True)
                             else:
                                 mask_value = src.dataset_mask(window=w, boundless=True)
 
-                            mask[w.row_off:w.row_off + w.height, w.col_off:w.col_off + w.width] = mask_value
+                            mask[
+                                w.row_off : w.row_off + w.height,
+                                w.col_off : w.col_off + w.width,
+                            ] = mask_value
 
                     mem.write_mask(mask)
 
-                    overviews = [2**j for j in range(1, overview_level + 1)]
+                    overviews = [2 ** j for j in range(1, overview_level + 1)]
                     mem.build_overviews(overviews, Resampling.nearest)
-                    mem.update_tags(ns='rio_overview', resampling=Resampling.nearest.value)
+                    mem.update_tags(
+                        ns="rio_overview", resampling=Resampling.nearest.value
+                    )
 
                     copy(mem, dst_path, copy_src_overviews=True, **dst_kwargs)
