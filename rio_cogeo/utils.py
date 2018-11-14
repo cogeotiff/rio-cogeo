365e768339dc3a6a5e8eafc509df03e08f95b28e
vincentsarago <vincent.sarago@gmail.com>
2018-11-14 00:18:10 -0500

    utility functions

---
+++ rio_cogeo/utils.py
@@ -0,0 +1,32 @@
+"""rio_cogeo.utils: Utility functions."""
+
+import math
+
+from rasterio.warp import calculate_default_transform
+
+
+def _meters_per_pixel(zoom, lat):
+    """Return the pixel resolution for a given mercator tile zoom and lattitude."""
+    return (math.cos(lat * math.pi / 180.0) * 2 * math.pi * 6378137) / (256 * 2 ** zoom)
+
+
+def get_max_zoom(src, snap=0.5, max_z=23):
+    """Calculate raster max zoom level."""
+    dst_affine, w, h = calculate_default_transform(
+        src.crs, "epsg:3857", src.width, src.height, *src.bounds
+    )
+
+    res_max = max(abs(dst_affine[0]), abs(dst_affine[4]))
+
+    tgt_z = max_z
+    mpp = 0.0
+
+    # loop through the pyramid to file the closest z level
+    for z in range(1, max_z):
+        mpp = _meters_per_pixel(z, 0)
+
+        if (mpp - ((mpp / 2) * snap)) < res_max:
+            tgt_z = z
+            break
+
+    return tgt_z
