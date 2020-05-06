c580d2d80a89eda8c9c67259075d43fd0692dacf
vincentsarago <vincent.sarago@gmail.com>
2020-05-06 18:33:06 -0400

    remove python 2

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ if sys.version_info >= (3, 6):
 
 setup(
     name="rio-cogeo",
-    version="1.1.10",
+    version="2.0a1",
+    python_requires=">=3",
     description=u"CloudOptimized GeoTIFF (COGEO) creation plugin for rasterio",
     long_description=readme,
     long_description_content_type="text/markdown",
