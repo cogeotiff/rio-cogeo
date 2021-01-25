745e41521a192b781767c843d935932ee3c9847d
vincentsarago <vincent.sarago@gmail.com>
2021-01-25 09:23:30 -0500

    add top level export

--- rio_cogeo/__init__.py
+++ rio_cogeo/__init__.py
@@ -1,3 +1,6 @@
 """rio_cogeo: Cloud Optimized GeoTIFF creation and validation plugin for rasterio."""
 
+from rio_cogeo.cogeo import cog_info, cog_translate, cog_validate  # noqa
+from rio_cogeo.profiles import cog_profiles  # noqa
+
 __version__ = "2.1.0"
