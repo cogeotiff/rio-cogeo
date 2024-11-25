ad2e854a16e01a946dacb2cccc6d42094f1b4208
vincentsarago <vincent.sarago@gmail.com>
2024-11-25 09:30:44 +0100

    Bump version: 5.3.6 → 5.4.0

--- rio_cogeo/__init__.py
+++ rio_cogeo/__init__.py
@@ -1,6 +1,6 @@
 """rio_cogeo: Cloud Optimized GeoTIFF creation and validation plugin for rasterio."""
 
-__version__ = "5.3.6"
+__version__ = "5.4.0"
 
 from .cogeo import cog_info, cog_translate, cog_validate  # noqa
 from .profiles import cog_profiles  # noqa
